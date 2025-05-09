de55a2985a2aa16946afe8acc3c47a3e499115bb
Robert Pozun <robert.pozun@croissant.com>
2025-05-09 08:03:35 -0400

    fix: exclude infrastructure from Next.js build process to resolve aws-cdk-lib import error

--- next.config.js
+++ next.config.js
@@ -9,6 +9,17 @@ const config = {
   images: {
     domains: ["i.scdn.co"],
   },
+  // Completely exclude the infrastructure directory from Next.js
+  // This prevents Next.js from trying to compile AWS CDK code
+  webpack: (config, { isServer }) => {
+    // Tell webpack to ignore the infrastructure directory
+    config.externals = [...(config.externals || []), 'aws-cdk-lib', 'constructs'];
+    return config;
+  },
+  // Only include directories that contain our application code
+  // This will prevent Next.js from processing the infrastructure directory
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'].map(ext => `page.${ext}`),
+  transpilePackages: [],
 };
 
 export default config;
